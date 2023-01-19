b6251673cfa7e6c15443967674e8e7a0012d05e3
frankzamma <frank581.fgz@gmail.com>
2023-01-19 21:50:16 +0100

    Aggiunto script per generazione di accordi e scale maggiori

---
+++ src/main/webapp/script/utilsChord.js
@@ -0,0 +1,37 @@
+function getChords(note){
+    let chords = [getMajorTriad(note)];
+
+    let quarter = Tone.Midi(note).transpose(5).toNote();
+    let quinta = Tone.Midi(note).transpose(7).toNote();
+
+    chords.push(getMajorTriad(quarter));
+    chords.push(getMajorTriad(quinta))
+
+    return chords;
+}
+
+function getMajorTriad(note){
+    let triad = [note];
+
+    let terza =  Tone.Midi(note).transpose(4).toNote();
+    let quinta =  Tone.Midi(terza).transpose(3).toNote();
+
+    triad.push(terza);
+    triad.push(quinta);
+
+    return triad;
+}
+
+function getScale(note){
+    let scale = [note];
+    let intervals =  [2, 2, 1, 2, 2, 2, 1];
+
+    for(let i = 0; i < 7; i++){
+        scale.push(Tone.Midi(scale[i]).transpose(intervals[i]).toNote());
+       console.log(intervals[i]);
+    }
+    console.log(scale);
+    return scale;
+}
+
+export{getChords, getScale}
