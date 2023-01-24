b6ba688f67b8782780c80818774d3b5e57f45941
frankzamma <frank581.fgz@gmail.com>
2023-01-24 13:10:27 +0100

    Spostate funzioni da utils.js a utilsForCca.js Eliminato utils.js

--- src/main/webapp/script/utilsForCca.js
+++ src/main/webapp/script/utilsForCca.js
@@ -1,5 +1,3 @@
-import {nmod} from "./utils.js";
-
 function contaVicini(array, i, j, state, row, col){
     let count = 0;
 
@@ -58,7 +56,54 @@ function generaConfigurazioneCasuale(array, row, col, p, color){
     }
 }
 
-export{contaVicini, generaConfigurazioneCasuale}
+
+function nmod(n, m) {
+    //console.log('(' + n + ',' + m + ')');
+    if(n >= 0){
+        //console.log(n%m);
+        return n%m;
+    }else{
+        //console.log(n + m);
+        return n + m
+    }
+}
+
+function creaGioco(row, col, cambiaStato, avviaGioco, stoppaGioco, array, color) {
+    let elem = document.querySelector('div#container-game');
+    let tab = document.createElement('table');
+    tab.setAttribute('id', 'compositor');
+
+    for(let i = 0; i < row; i++){
+        let tr = document.createElement('tr');
+        for(let j = 0; j < col; j++){
+
+            let tmp = document.createElement('td');
+            tmp.id = i + '-' + j;
+
+            if(array != undefined)
+                tmp.addEventListener('click',function (){cambiaStato(array, color, tmp)})
+            else
+                tmp.addEventListener('click',function (){cambiaStato(tmp)})
+            tr.appendChild(tmp)
+        }
+
+        tab.appendChild(tr);
+    }
+    elem.appendChild(tab)
+
+    let avviaButton =  document.getElementById('avvia-button');
+    avviaButton.addEventListener('click', avviaGioco);
+
+    let stopButton =  document.getElementById('stop-button');
+    stopButton.addEventListener('click', stoppaGioco);
+}
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+
+export{contaVicini, generaConfigurazioneCasuale, nmod, creaGioco, sleep}
 
 
 
